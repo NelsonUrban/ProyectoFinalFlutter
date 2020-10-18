7bd82a41aec8d5127d814d7f76cf0def44e704f4
Eddy Grullón <ecgrullon@gmail.com>
2020-10-18 12:28:47 -0400

    New style

--- proyecto_final/lib/main.dart
+++ proyecto_final/lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_final/src/pages/add_destinos_page.dart';
-
-import 'package:proyecto_final/src/routes/routes.dart';
+import 'package:proyecto_final/src/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Destinos Turisticos App',
+      title: 'Flutter Travel UI',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: getRoutes(),
-      onGenerateRoute: (RouteSettings settings) {
-        return MaterialPageRoute(
-            builder: (BuildContext context) => AddDestinos());
-      },
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ proyecto_final/lib/src/models/destination_model.dart
@@ -0,0 +1,82 @@
+import 'package:proyecto_final/src/models/activity_model.dart';
+
+class Destination {
+  String imageUrl;
+  String city;
+  String country;
+  String description;
+  List<Activity> activities;
+
+  Destination({
+    this.imageUrl,
+    this.city,
+    this.country,
+    this.description,
+    this.activities,
+  });
+}
+
+List<Activity> activities = [
+  Activity(
+    imageUrl: 'assets/images/punta-cana.jpg',
+    name: 'St. Mark\'s Punta Cana',
+    type: 'Sightseeing Tour',
+    startTimes: ['9:00 am', '11:00 am'],
+    rating: 5,
+    price: 30,
+  ),
+  Activity(
+    imageUrl: 'assets/images/gondola.jpg',
+    name: 'Walking Tour and Gonadola Ride',
+    type: 'Sightseeing Tour',
+    startTimes: ['11:00 pm', '1:00 pm'],
+    rating: 4,
+    price: 210,
+  ),
+  Activity(
+    imageUrl: 'assets/images/murano.jpg',
+    name: 'Murano and Burano Tour',
+    type: 'Sightseeing Tour',
+    startTimes: ['12:30 pm', '2:00 pm'],
+    rating: 3,
+    price: 125,
+  ),
+];
+
+List<Destination> destinations = [
+  Destination(
+    imageUrl: 'assets/images/punta-cana.jpg',
+    city: 'Punta Cana',
+    country: 'Rep. Dom',
+    description: 'Visita nuestras playas.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/images/bayahibe.jpg',
+    city: 'Bayahibe',
+    country: 'Rep. Dom',
+    description: 'Visit Paris for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/images/puerto-plata.jpg',
+    city: 'Puerto Plata',
+    country: 'Rep. Dom',
+    description: 'Visit New Delhi for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/images/saopaulo.jpg',
+    city: 'Sao Paulo',
+    country: 'Brazil',
+    description: 'Visit Sao Paulo for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+  Destination(
+    imageUrl: 'assets/images/newyork.jpg',
+    city: 'New York City',
+    country: 'United States',
+    description: 'Visit New York for an amazing and unforgettable adventure.',
+    activities: activities,
+  ),
+];
