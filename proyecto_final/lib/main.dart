5300d28a879732e130b8c6db8578ff2584d98a52
Eddy Grullón <ecgrullon@gmail.com>
2020-10-18 13:21:28 -0400

    minor changes

--- proyecto_final/lib/main.dart
+++ proyecto_final/lib/main.dart
@@ -9,14 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Travel UI',
+      title: 'Turismo RD',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFF3EBACE),
         accentColor: Color(0xFFD8ECF1),
         scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
-      home: MyHomePage(),
+      home: HomeScreen(),
     );
   }
 }
